12e1d5a078dbd7e2460a73b351ac5e6ac6d69b13
YassineFassi <yassine.fassi@gmail.com>
2019-11-18 16:57:42 +0100

    grosse MAJ

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-   
+   <link rel="stylesheet" href="style.css">
     <title>TechInfo - Acceuil</title>
 </head>
 <header>
@@ -46,7 +46,6 @@ include "header.php";
 
 </div>
 
-<a href="reparations.php" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Réparations</a>
 
 <footer>
 <?php
