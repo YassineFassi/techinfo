5d4a425e997e04a3012775c8750bff54f0941dd4
Uranie31 <57894146+Uranie31@users.noreply.github.com>
2019-11-18 17:07:55 +0100

    css footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
-<footer>
+<link rel="stylesheet" href="style.css">
+<div class="footer">
 <a href="contact.php" class="btn btn-info btn-lg active" role="button" aria-pressed="true">Contact</a>
 <a href="presentation.php" class="btn btn-info btn-lg active" role="button" aria-pressed="true">Qui sommes-nous ?</a>
 <a href="index.php" class="btn btn-info btn-lg active">Retour à la page d'acceuil</a>
-
-</footer>
+</div>
