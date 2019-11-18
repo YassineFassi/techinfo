a22c801bc7f3c07109f1321336c766f43dddfbde
YassineFassi <yassine.fassi@gmail.com>
2019-11-18 14:32:15 +0100

    mise à jour navbar

--- index.php
+++ index.php
@@ -5,12 +5,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
     <title>TechInfo - Acceuil</title>
 </head>
+
+<header>
+	
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Menu</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Acceuil <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="reparations.php">Réparations</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="presentation.php" tabindex="-1" aria-disabled="true">Qui sommes-nous ?</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+		</header>
+        
 <body>
     <h1>Bienvenue chez TechInfo</h1>
 
-    <a href="reparations.php">Réparations</a>
- 
+
+<a href="reparations.php" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Réparations</a>
+<hr>
+
+<footer>
+<a href="presentation.php" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Contact</a>
+<a href="presentation.php" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Qui sommes-nous ?</a>
+
+</footer>
+
+
 </body>
 </html>
