e8f0408db501123cb893d9c2c4689e52a312a3e9
Uranie31 <57894146+Uranie31@users.noreply.github.com>
2019-11-18 15:20:42 +0100

    mise à jour

--- reparations.php
+++ reparations.php
@@ -14,28 +14,7 @@
 <body>
 <header>
 	
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Menu</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Acceuil <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="reparations.php">Réparations</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="contact.php">Montage PC</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="presentation.php" tabindex="-1" aria-disabled="true">Vente de produits</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+
 		</header>
 <h1>Devis pour Réparations</h1><hr>
 
@@ -69,8 +48,7 @@
 
 
 <footer>
-<a href="contact.php" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Contact</a>
-<a href="presentation.php" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Qui sommes-nous ?</a>
+
 </footer>
 </body>
 </html>
