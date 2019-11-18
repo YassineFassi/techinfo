1ca8fd998cf67d21afd3b22d1f99f1cf7cc78306
YassineFassi <yassine.fassi@gmail.com>
2019-11-18 14:12:31 +0100

    ajout du menu principal

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
-<?php
 
-echo "hello world";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TechInfo - Acceuil</title>
+</head>
+<body>
+    <h1>Bienvenue chez TechInfo</h1>
 
-?>
+    <a href="reparations.php">Réparations</a>
+ 
+</body>
+</html>
